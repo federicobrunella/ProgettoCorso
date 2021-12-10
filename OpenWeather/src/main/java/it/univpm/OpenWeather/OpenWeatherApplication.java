14df5f2d5127c8c1a5e81c5babd1c8641a03b0d2
federicobrunella <48298111+federicobrunella@users.noreply.github.com>
2021-12-10 15:04:17 +0100

    Creazione Progettto

---
+++ OpenWeather/src/main/java/it/univpm/OpenWeather/OpenWeatherApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.OpenWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenWeatherApplication.class, args);
+	}
+
+}
