3b804f33d1403e011f55dd610e51422bab50fcac
LeonardoPieralisi <94040109+LeonardoPieralisi@users.noreply.github.com>
2021-12-13 16:05:22 +0100

    Statistics: bozza classi per statistiche

--- OpenWeather/src/main/java/it/univpm/OpenWeather/Statistics/AdvancedStatisticsImpl.java
+++ OpenWeather/src/main/java/it/univpm/OpenWeather/Statistics/AdvancedStatisticsImpl.java
@@ -1,10 +1,33 @@
 package it.univpm.OpenWeather.Statistics;
 
+import org.json.simple.JSONObject;
+
+import it.univpm.OpenWeather.model.City;
+
 /**
 * @author Leonardo Pieralisi
 *
 */
 
 public class AdvancedStatisticsImpl extends Statistics {
+	
+	JSONObject obj;
+	
+	public AdvancedStatisticsImpl(City city, JSONObject obj) {
+		
+		this.city=city;
+		this.obj=obj;
+		//TODO:...
+	}
 
+	public JSONObject getJSONStatistics() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void calculateStatistics() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
--- OpenWeather/src/main/java/it/univpm/OpenWeather/Statistics/StatisticsImpl.java
+++ OpenWeather/src/main/java/it/univpm/OpenWeather/Statistics/StatisticsImpl.java
@@ -1,10 +1,50 @@
 package it.univpm.OpenWeather.Statistics;
 
+import org.json.simple.JSONObject;
+
+import it.univpm.OpenWeather.model.City;
+
 /**
 * @author Leonardo Pieralisi
 *
 */
 
 public class StatisticsImpl extends Statistics {
+	
+	private int days;
+	private int forecastCount;
+	
+	public StatisticsImpl(City city, String days) {
+		
+		this.city=city;
+		this.days=Integer.valueOf(days);
+		this.forecastCount=this.days*8;  //ogni giorno ricevo le previsioni ogni 3 ore...
+	}
+	
+	public JSONObject getJSONStatistics() {
+		//TODO: fare l'output
+		return null;
+	}
+	
+	public void calculateStatistics() {
+		//TODO: calcolo
+	}
 
+	public int getDays() {
+		return days;
+	}
+
+	public void setDays(int days) {
+		this.days = days;
+	}
+
+	public int getForecastCount() {
+		return forecastCount;
+	}
+
+	public void setForecastCount(int forecastCount) {
+		this.forecastCount = forecastCount;
+	}
+	
+	
 }
