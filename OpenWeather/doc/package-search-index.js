37c3fd12d88aff3b386e11bd01e4def16b6dfc27
LeonardoPieralisi <94040109+LeonardoPieralisi@users.noreply.github.com>
2021-12-20 14:57:25 +0100

    Generata JavaDoc

--- OpenWeather/doc/package-search-index.js
+++ OpenWeather/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"it.univpm.OpenWeather.model"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"it.univpm.OpenWeather"},{"l":"it.univpm.OpenWeather.controller"},{"l":"it.univpm.OpenWeather.exceptions"},{"l":"it.univpm.OpenWeather.filters"},{"l":"it.univpm.OpenWeather.model"},{"l":"it.univpm.OpenWeather.service"},{"l":"it.univpm.OpenWeather.statistics"}];updateSearchResults();
